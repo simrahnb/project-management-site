3c5923bb3283fae69dc7e256802e097a2d5d8804
sim <simrahnajeeb@gmail.com>
2023-02-15 10:23:06 +0500

    Implemented functionality to implement file inputs

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,53 +1,79 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // styles
-import './Signup.css'
+import "./Signup.css";
 
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbNail] = useState(null);
+  const [thumbnailError, setThumbNailError] = useState(null);
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [displayName, setDisplayName] = useState('')
-  const [thumbnail, setThumbNail] = useState(null)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbNail(null);
+    let selected = e.target.files[0]; // returns an array of files
+    console.log(selected);
+
+    if (!selected) {
+      setThumbNailError("Please select a file");
+      return;
+    }
+    if (!selected.type.includes("image")) {
+      setThumbNailError("Selected file must be an image");
+      return;
+    }
+    if (selected.size > 100000) {
+      setThumbNailError("Image file size must be less than 100kb");
+      return;
+    }
+
+    setThumbNailError(null);
+    setThumbNail(selected);
+    console.log("thumbnail updated");
+  };
 
   return (
-    <form className="auth-form">
-        <h2>Sign up</h2>
-        <label>
-          <span>email:</span>
-          <input 
-            required
-            type="email"
-            onChange={(e) => setEmail(e.target.value)} 
-            value={email}
-            />
-        </label>
-        <label>
-          <span>password:</span>
-          <input 
-            required
-            type="password"
-            onChange={(e) => setPassword(e.target.value)} 
-            value={password}
-            />
-        </label>
-        <label>
-          <span>display name:</span>
-          <input 
-            required
-            type="text"
-            onChange={(e) => setDisplayName(e.target.value)} 
-            value={displayName}
-            />
-        </label>
-        <label>
-          <span>profile thumbnail:</span>
-          <input 
-            required
-            type="file"
-            />
-        </label>
-        <button className="btn">Sign up</button>
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>profile thumbnail:</span>
+        <input required type="file" onChange={handleFileChange} />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
+      </label>
+      <button className="btn">Sign up</button>
     </form>
-  )
+  );
 }
