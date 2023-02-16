b60a49292bfee1db671b4bcb95bdf312590e0683
sim <simrahnajeeb@gmail.com>
2023-02-16 12:29:02 +0500

    Implemented functionality to log users in

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,44 @@
+import React, { useState } from 'react'
+import { useLogin } from '../../hooks/useLogin'
+
 // styles
 import './Login.css'
 
-import React from 'react'
-
 export default function Login() {
+
+  const [email, setEmail] = useState('') 
+  const [password, setPassword] = useState('')
+  const { login, error, isPending } = useLogin()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    login(email,password)
+  }
+
   return (
-    <div>
-        Login
-    </div>
+    <form className="auth-form" onSubmit={handleSubmit}>
+    <h2>Login</h2>
+    <label>
+      <span>email:</span>
+      <input
+        required
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+    </label>
+    <label>
+      <span>password:</span>
+      <input
+        required
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+    </label>
+    {!isPending && <button className="btn">Login</button>}
+    {isPending && <button className="btn" disabled>Loading</button>}
+    {error && <div className="error">{error}</div>}
+  </form>
   )
 }
