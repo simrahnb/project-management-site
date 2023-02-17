5a69a7b8b6fd7d941a9cb54c7570810fdd557f14
sim <simrahnajeeb@gmail.com>
2023-02-17 12:34:43 +0500

    Implemented redirects & conditional nav bar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // styles & images
 import './Navbar.css'
@@ -8,6 +9,7 @@ import Temple from '../assets/temple.svg'
 export default function Navbar() {
 
   const { logout, isPending } = useLogout()
+  const { user } = useAuthContext()
 
   return (
     <nav className="navbar">
@@ -17,12 +19,19 @@ export default function Navbar() {
           <span>The Site</span>
         </li>
 
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
+        {!user && (
+        <>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Signup</Link></li>
+        </>
+        )}
+
+        {user && (
         <li>
           {!isPending && <button className="btn" onClick={logout}>Logout</button>}
           {isPending && <button className="btn" disabled>Logging out..</button>}
         </li>
+        )}
       </ul>
     </nav>
   )
