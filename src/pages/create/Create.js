33c5c660fc039263cb4197852ef188a7e4ed1082
sim <simrahnajeeb@gmail.com>
2023-02-18 00:40:39 +0500

    Added project category option to create project form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,15 @@
 import { useState } from "react";
+import Select from "react-select";
 
 // styles
 import "./Create.css";
+
+const categories = [
+  { value: "development", label: "Development" },
+  { value: "design", label: "Design" },
+  { value: "sales", label: "Sales" },
+  { value: "marketing", label: "Marketing" },
+];
 
 export default function Create() {
   // form fields
@@ -13,7 +21,7 @@ export default function Create() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name, details, dueDate);
+    console.log(name, details, dueDate, category.value);
   };
 
   return (
@@ -49,7 +57,10 @@ export default function Create() {
         </label>
         <label>
           <span>Project category:</span>
-          {/* category select here */}
+          <Select
+            onChange={(option) => setCategory(option)}
+            options={categories}
+          />
         </label>
         <label>
           <span>Assign to:</span>
