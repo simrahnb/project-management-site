4da5caeeb6c49ce80ea6a29efef2a7a4c80b805e
sim <simrahnajeeb@gmail.com>
2023-02-18 21:53:31 +0500

    Can add comments now on project details page

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,27 +1,28 @@
 import { useParams } from "react-router-dom";
 import { useDocument } from "../../hooks/useDocument";
-import ProjectSummary from "./ProjectSummary";
+
+// components
 import ProjectComments from "./ProjectComments";
+import ProjectSummary from "./ProjectSummary";
 
 // styles
 import "./Project.css";
 
 export default function Project() {
   const { id } = useParams();
-  const { error, document } = useDocument("projects", id);
+  const { document, error } = useDocument("projects", id);
 
   if (error) {
     return <div className="error">{error}</div>;
   }
-
   if (!document) {
     return <div className="loading">Loading...</div>;
   }
 
   return (
     <div className="project-details">
-      <h1><ProjectSummary project={document} /></h1>
-      <ProjectComments />
+      <ProjectSummary project={document} />
+      <ProjectComments project={document} />
     </div>
   );
 }
