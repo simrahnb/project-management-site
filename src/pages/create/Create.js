f7dc158d8554b8b5af4649320c945973371373d5
sim <simrahnajeeb@gmail.com>
2023-02-18 18:03:13 +0500

    Can save projects now

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -3,6 +3,8 @@ import Select from "react-select";
 import { useCollection } from '../../hooks/useCollection';
 import { timestamp } from "../../firebase/config";
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useFirestore } from "../../hooks/useFirestore";
+import { useHistory } from "react-router-dom";
 
 // styles
 import "./Create.css";
@@ -15,7 +17,8 @@ const categories = [
 ];
 
 export default function Create() {
-
+  const history = useHistory()
+  const { addDocument, response } = useFirestore('projects');
   const { documents } = useCollection('users');
   const [users, setUsers] = useState([]);
   const { user } = useAuthContext();
@@ -37,7 +40,7 @@ export default function Create() {
     }
   }, [documents])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setFormError(null)
 
@@ -74,7 +77,10 @@ export default function Create() {
       assignedUsersList 
     }
 
-    console.log(project);
+    await addDocument(project)
+    if (!response.error) {
+      history.push('/')
+    }
   };
 
   return (
