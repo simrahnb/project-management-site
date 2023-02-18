975bb63444659ce07ba1827a9bc74f01a437dee9
sim <simrahnajeeb@gmail.com>
2023-02-18 18:13:33 +0500

    Able to fetch projects and display them on the dashboard

---
+++ src/components/ProjectList.js
@@ -0,0 +1,15 @@
+// styles
+import "./ProjectList.css";
+
+import React from "react";
+
+export default function ProjectList({ projects }) {
+  return (
+    <div>
+      {projects.length === 0 && <p>No projects yet</p>}
+      {projects.map((project) => (
+        <div key={project.id}>{project.name} </div>
+      ))}
+    </div>
+  );
+}
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,12 +1,17 @@
-// styles
-import './Dashboard.css'
+import ProjectList from "../../components/ProjectList";
+import { useCollection } from "../../hooks/useCollection";
 
-import React from 'react'
+// styles
+import "./Dashboard.css";
 
 export default function Dashboard() {
+  const { documents, error } = useCollection("projects");
+
   return (
     <div>
-        Dashboard
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
     </div>
-  )
+  );
 }
