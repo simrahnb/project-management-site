89990073d1a85ca92640dcad6a0ecb7561982317
sim <simrahnajeeb@gmail.com>
2023-02-19 00:30:06 +0500

    Implementing functionality to filter projects

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import ProjectList from "../../components/ProjectList";
 import { useCollection } from "../../hooks/useCollection";
+import ProjectFilter from "./ProjectFilter";
 
 // styles
 import "./Dashboard.css";
@@ -11,6 +12,7 @@ export default function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   );
---
+++ src/pages/dashboard/ProjectFilter.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const filterlist = [
+  "all",
+  "mine",
+  "development",
+  "design",
+  "marketing",
+  "sales",
+];
+
+export default function ProjectFilter() {
+  const [currentFilter, setCurrentFilter] = useState("all");
+
+  const handleClick = (newFilter) => {
+    console.log(newFilter);
+    setCurrentFilter(newFilter);
+  };
+
+  return (
+    <div className="project-filter">
+      <nav>
+        {filterlist.map((f) => (
+          <button
+            key={f}
+            onClick={() => handleClick(f)}
+            className={currentFilter === f ? "active" : ""}
+          >
+            {f}
+          </button>
+        ))}
+      </nav>
+    </div>
+  );
+}
