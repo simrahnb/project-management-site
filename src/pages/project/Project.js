7c1df842a091baba7d8be7dd8f8379dd0f5352e1
sim <simrahnajeeb@gmail.com>
2023-02-18 21:28:53 +0500

    Made a comments form

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useDocument } from "../../hooks/useDocument";
 import ProjectSummary from "./ProjectSummary";
+import ProjectComments from "./ProjectComments";
 
 // styles
 import "./Project.css";
@@ -20,6 +21,7 @@ export default function Project() {
   return (
     <div className="project-details">
       <h1><ProjectSummary project={document} /></h1>
+      <ProjectComments />
     </div>
   );
 }
